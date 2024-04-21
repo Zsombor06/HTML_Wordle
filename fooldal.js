110330c49153de000aae46b5d2d552d9467b2d58
Zsombor06 <simon.zsombor06@gmail.com>
2024-04-21 09:48:50 +0200

    Fix

--- fooldal.js
+++ fooldal.js
@@ -30,401 +30,127 @@ $("Q").addEventListener("click",()=>{
     if(szo.length<5){
     szo.push("Q")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]
-                }
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("W").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("W")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("E").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("E")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("R").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("R")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("T").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("T")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("Z").addEventListener("click",()=>{
     if(szo.length<5){  szo.push("Z")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("U").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("U")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("I").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("I")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("O").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("O")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("P").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("P")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("A").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("A")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("S").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("S")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("D").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("D")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("F").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("F")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("G").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("G")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("H").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("H")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("J").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("J")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("K").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("K")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("L").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("L")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("Y").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("Y")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("X").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("X")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("C").addEventListener("click",()=>{
@@ -449,77 +175,25 @@ $("C").addEventListener("click",()=>{
 $("V").addEventListener("click",()=>{
     if(szo.length<5){ szo.push("V")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("B").addEventListener("click",()=>{
     if(szo.length<5){szo.push("B")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("N").addEventListener("click",()=>{
     if(szo.length<5){szo.push("N")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("M").addEventListener("click",()=>{
     if(szo.length<5){szo.push("M")
     hely="box"+sor+""+oszlop
-    if($(hely).innerHTML != ""){
-        if($(hely).innerHTML !=""){
-            if($(hely).innerHTML != ""){
-                if($(hely).innerHTML !=""){
-                    $(hely).innerHTML = szo[oszlop]
-        }
-                else{$(hely).innerHTML = szo[oszlop]}
-       
-
-    }else{$(hely).innerHTML = szo[oszlop]}}
-    else{
-        $(hely).innerHTML = szo[oszlop]
-    }}
-    else{$(hely).innerHTML = szo[oszlop]}
+    $(hely).innerHTML=szo[oszlop]
     oszlop++}
 })
 $("DEL").addEventListener("click",()=>{
